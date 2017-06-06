a550f29b61ef9cafde15d218ce1107f235bc5bb5
shawbena <shaw@shawbenadeMacBook-Pro.local>
2017-06-06 23:02:28 +0800

    update

--- guid/public/dist/handling_events/app.js
+++ guid/public/dist/handling_events/app.js
@@ -1,101 +1,101 @@
 define([], function () {
-  'use strict';
+    'use strict';
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  var _createClass = function () {
-    function defineProperties(target, props) {
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-      }
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
 
-    return function (Constructor, protoProps, staticProps) {
-      if (protoProps) defineProperties(Constructor.prototype, protoProps);
-      if (staticProps) defineProperties(Constructor, staticProps);
-      return Constructor;
-    };
-  }();
+    var _createClass = function () {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
 
-  function _possibleConstructorReturn(self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        return function (Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
+
+    function _possibleConstructorReturn(self, call) {
+        if (!self) {
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        }
+
+        return call && (typeof call === "object" || typeof call === "function") ? call : self;
     }
 
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  }
+    function _inherits(subClass, superClass) {
+        if (typeof superClass !== "function" && superClass !== null) {
+            throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+        }
 
-  function _inherits(subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  }
-
-  function ActionLink() {
-    return React.createElement(
-      'a',
-      { href: '#', onClick: handleClick },
-      'Click'
-    );
-
-    function handleClick(e) {
-      e.preventDefault();
-      console.log('The link was clicked.');
-    }
-  }
-
-  var Toggle = function (_React$Component) {
-    _inherits(Toggle, _React$Component);
-
-    function Toggle(props) {
-      _classCallCheck(this, Toggle);
-
-      var _this = _possibleConstructorReturn(this, (Toggle.__proto__ || Object.getPrototypeOf(Toggle)).call(this, props));
-
-      _this.handleClick = function (e) {
-        console.log(e);
-        _this.setState(function (prevState) {
-          return {
-            isToggleOn: !prevState.isToggleOn
-          };
+        subClass.prototype = Object.create(superClass && superClass.prototype, {
+            constructor: {
+                value: subClass,
+                enumerable: false,
+                writable: true,
+                configurable: true
+            }
         });
-      };
-
-      _this.state = { isToggleOn: true };
-      return _this;
+        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
     }
 
-    _createClass(Toggle, [{
-      key: 'render',
-      value: function render() {
+    function ActionLink() {
         return React.createElement(
-          'button',
-          { onClick: this.handleClick },
-          this.state.isToggleOn ? 'ON' : 'OFF'
+            'a',
+            { href: '#', onClick: handleClick },
+            'Click'
         );
-      }
-    }]);
 
-    return Toggle;
-  }(React.Component);
+        function handleClick(e) {
+            e.preventDefault();
+            console.log('The link was clicked.');
+        }
+    }
 
-  ReactDOM.render(React.createElement(Toggle, null), document.getElementById('app'));
+    var Toggle = function (_React$Component) {
+        _inherits(Toggle, _React$Component);
+
+        function Toggle(props) {
+            _classCallCheck(this, Toggle);
+
+            var _this = _possibleConstructorReturn(this, (Toggle.__proto__ || Object.getPrototypeOf(Toggle)).call(this, props));
+
+            _this.handleClick = function (e) {
+                console.log(e);
+                _this.setState(function (prevState) {
+                    return {
+                        isToggleOn: !prevState.isToggleOn
+                    };
+                });
+            };
+
+            _this.state = { isToggleOn: true };
+            return _this;
+        }
+
+        _createClass(Toggle, [{
+            key: 'render',
+            value: function render() {
+                return React.createElement(
+                    'button',
+                    { onClick: this.handleClick },
+                    this.state.isToggleOn ? 'ON' : 'OFF'
+                );
+            }
+        }]);
+
+        return Toggle;
+    }(React.Component);
+
+    ReactDOM.render(React.createElement(Toggle, null), document.getElementById('app'));
 });
