9fcc7d7a8232ffb1b8db061e0effcea3f146a68a
shawbena <shaw@192.168.0.100>
2018-02-02 23:11:04 +0800

    add typechecking to "bootstrap" function

--- bootstrap.tsx
+++ bootstrap.tsx
@@ -7,7 +7,7 @@ import { render } from 'react-dom';
  * @param props 
  * @param container
  */
-export default function bootstrap(App:any, props?:any, container?: Element){
+export default function bootstrap<P={}>(App:React.ReactType<P>, props?:P, container?: Element){
     // props 类型可以更具体些
     // App 也许也可以
     if(!container){
