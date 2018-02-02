be351b2bad80622ef6f246ec80dd5863a962a106
shawbena <shaw@192.168.0.100>
2018-02-02 23:13:13 +0800

    update

--- bootstrap.tsx
+++ bootstrap.tsx
@@ -8,8 +8,6 @@ import { render } from 'react-dom';
  * @param container
  */
 export default function bootstrap<P={}>(App:React.ReactType<P>, props?:P, container?: Element){
-    // props 类型可以更具体些
-    // App 也许也可以
     if(!container){
         container = document.createElement('div')
         document.body.appendChild(container)
